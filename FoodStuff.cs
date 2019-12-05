675c042856b5ea99847742082e9a17c9525eac03
jwen8545 <jwen8545@gmail.com>
2019-12-05 18:08:07 -0500

    lotta ui updates

---
+++ FoodStuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTest.Shared
+{
+    public class FoodStuff : ComponentBase
+    {
+        public string IngredientID { get; set; }
+        public string IngredientName { get; set; }
+        public int count { get; set; }
+
+        protected IList<FoodStuff> ings = new List<FoodStuff>();
+        protected string newTodo = "1";
+        protected string newTodo1 = "Eggs";
+        protected int counter = 8;
+        
+        protected void populateList()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                ings.Add(new FoodStuff { IngredientID = newTodo, IngredientName = newTodo1, count = counter });
+                ings.Add(new FoodStuff { IngredientID = "2", IngredientName = "Chicken", count = 5 });
+                ings.Add(new FoodStuff { IngredientID = "3", IngredientName = "Rice", count = 10 });
+                newTodo = string.Empty;
+                
+            }
+        }
+    }
+}
--- TodoItem.cs
+++ TodoItem.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorTest.Shared
 {
-    public class TodoItem
+    public class TodoItem : ComponentBase
     {
         public string Title { get; set; }
         public bool IsDone { get; set; }
+
+        protected IList<TodoItem> todos = new List<TodoItem>();
+        protected string newTodo;
+
+        protected void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
     }
 }
